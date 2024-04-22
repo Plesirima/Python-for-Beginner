6c2133cd62d47b4df6427d449352ee1bde90c2bc
Plesirima <160445892+Plesirima@users.noreply.github.com>
2024-04-22 18:36:24 +0700

    [Created] เพิ่ม Folder แบบฝึกวันที่1

---
+++ Practiceday1/practice3.py
@@ -0,0 +1,18 @@
+#Basic Data Type
+#แบบฝึกหัด 1-4 ข้อที่ 1
+# ให้ m เก็บจำนวนเต็มมีค่า 12
+# ให้ x เก็บจำนวนจริงมีค่า 12
+# ให้ t เก็บสตริง ABC
+# ให้ q เก็บค่าจริง
+# ให้ a เก็บรายการของ 7, 9, 5
+
+m = 12
+x = 12.0
+t = "ABC"
+q = True
+a = [7,9,5]
+print(type (m))
+print(type (x))
+print(type (t))
+print(type (q))
+print(type (a))
